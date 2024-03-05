326da5d0a62b5f8f5974fa3d8cd06c7acded6d4f
willofD13 <leonidastsiakas13@gmail.com>
2024-03-05 17:41:33 +0200

    Delete restaurant dir

--- restaurant/src/menu.js
+++
@@ -1,7 +0,0 @@
-import { breakfastMenu } from "./breakfast";
-import { sidesMenu } from "./sides";
-
-export { breakfastMenu }
-export { sidesMenu }
-const content = document.getElementById('content')
-content.classList.remove('content')
--- restaurant/src/text.js
+++
@@ -1,8 +0,0 @@
-(function addText() {
-    const content = document.getElementById('content');
-
-    const text = document.createElement('div');
-    text.textContent = 'Explore your tastes with us'
-
-    content.appendChild(text)
-})();
